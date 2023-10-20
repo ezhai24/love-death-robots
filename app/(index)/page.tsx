7f12d2e5a10fefc8399048a8dec5bc07b8cc0ce2
Emily Zhai <ezhai24@gmail.com>
2023-10-19 20:56:29 -0400

    set up basic three scene

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { Box, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 export default function Home() {
   return (
-    <main>
-      <div className="text-3xl font-bold underline">Hello!</div>
-    </main>
+    <Canvas className="h-screen">
+      <Box />
+      <OrbitControls />
+    </Canvas>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="h-screen">
+      <body className="h-screen">{children}</body>
     </html>
-  )
+  );
 }

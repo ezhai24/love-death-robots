367560f97ce45a2b1a440b067d5c5a7acf01980f
Emily Zhai <ezhai24@gmail.com>
2023-10-20 16:24:16 -0400

    add navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+
+import { externalRoutes } from "./routes";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +10,33 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-screen">
-      <body className="h-screen">{children}</body>
+      <body className="h-screen">
+        <nav className="absolute z-10 flex w-full justify-between px-[5%] pt-5">
+          <a
+            href={externalRoutes.netflix}
+            className=" left-[5%] top-5 z-10 w-5 md:w-8"
+          >
+            <Image
+              src="/assets/netflixLogo.png"
+              alt="Netflix logo"
+              height={60}
+              width={30}
+            />
+          </a>
+          <a
+            href={externalRoutes.netflixLDRTitle}
+            className=" right-[5%] top-5 z-10 w-28 md:w-44"
+          >
+            <Image
+              src="/assets/ldrIconLogo.svg"
+              alt="Heart, X, and robot icons of the Love, Death & Robots logo"
+              height={60}
+              width={200}
+            />
+          </a>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,4 @@
 export const externalRoutes = {
   netflixLDRTitle: "https://www.netflix.com/title/80174608",
+  netflix: "https://www.netflix.com/",
 };

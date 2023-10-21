abcef7f2a9c5c17983e1ba28098b14ede2e82e0e
Emily Zhai <ezhai24@gmail.com>
2023-10-21 11:29:19 -0400

    recenter camera to canvas origin so it's easier to get accurate viewport measurements

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -9,10 +9,7 @@ import TrailerModal from "./TrailerModal";
 export default function Home() {
   return (
     <>
-      <Canvas
-        className="h-screen"
-        camera={{ fov: 50, position: [0, 13.5, 5], rotation: [0, 0, 0] }}
-      >
+      <Canvas className="h-screen" camera={{ fov: 50 }}>
         <Environment files="/assets/abandoned-city.hdr" background blur={0.1} />
         <directionalLight intensity={1.5} position={[4, 0, 5]} />
 

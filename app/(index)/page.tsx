549869a9646431bcaca5ef27259bdcbe27963769
Emily Zhai <ezhai24@gmail.com>
2023-10-21 21:14:28 -0400

    loading screen

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { AnimatePresence } from "framer-motion";
+
+import { useProgress } from "@react-three/drei";
+
+import FullPageLoader from "@/app/components/FullPageLoader";
+
 import Canvas from "./Canvas";
 import TrailerModal from "./TrailerModal";
 
 export default function Home() {
+  const { progress } = useProgress();
+
   return (
     <>
+      <AnimatePresence>
+        {progress < 100 && <FullPageLoader key="loader" />}
+      </AnimatePresence>
+
       <Canvas />
       <TrailerModal className="absolute bottom-10 left-[5%] z-10" />
     </>
